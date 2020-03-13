e710887d86deb77e281af63534187c55d08e0cb9
kimjinhyeok <jhkim@synctechno.com>
2020-03-13 10:37:52 +0900

    gulp 설치 후 테스트파일

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,29 @@
-var serires = require('gulp');
+var gulp = require('gulp');
 var git = require('gulp-git');
-
-function defaultTask() {
-  cb();
-}
+var install = require('gulp-install');
 
 function updateSubmodules() {
-  git.updateSubmodule();
+  return new Promise(function(resolve, reject) {
+    try {
+      git.updateSubmodule();
+      resolve();
+    }catch(err) {
+      reject(err);
+    }
+  })
 }
-exports.deafult = serires(defaultTask, updateSubmodules);
+function initNpm() {
+  return new Promise(function(resolve, reject) {
+    gulp.src([
+        './webportal/package.json', 
+        './telegrambotmanagementassistserver/package.json', 
+        './otadevelopmentassistserver/package.json', 
+        './dashboard/package.json'
+      ])
+      .pipe(install())
+      .on('error', reject)
+      .pipe(gulp.dest("./"))
+      .on('end', resolve);
+  })
+}
+exports.default = gulp.series(updateSubmodules, initNpm);
